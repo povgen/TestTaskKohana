6f9009ed1cc6e884e0f5bbcb807332f2d97c6e77
povgen <povgen@yandex.ru>
2024-02-07 21:58:51 +0100

    add page layout

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -1,10 +1,29 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Welcome extends Controller {
+class Controller_Welcome extends Controller_Template {
 
+	public $template = 'layouts/base';
 	public function action_index()
 	{
 		$this->response->body('hello, world!');
+	}
+
+	public function action_test()
+	{
+//		var_dump(Auth::instance()->hash('123'));
+//		$res = Auth::instance()->login('admin', '123');
+//		$res = Auth::instance()->get_user();
+//		var_dump($res);
+//		die();
+
+//		$layout =
+
+		$this->template->content = View::factory('pages/login')
+			->set('places', array('Rome', 'Paris', 'London', 'New York', 'Tokyo'));
+
+		// The view will have $places and $user variables
+//		$this->response->body($view);
+
 	}
 
 } // End Welcome
---
+++ application/views/pages/login.php
@@ -0,0 +1,3 @@
+<? foreach ($places as $place): ?>
+	<div><?= $place ?></div>
+<? endforeach; ?>
